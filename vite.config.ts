bd79aaf99aa24363369e456cb13a3d7a03de0ff5
Santiago Hernandez <santiagooheernandez@gmail.com>
2022-12-21 10:40:03 -0400

    Refactor: add routes's aliases

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,18 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@utils": path.resolve(__dirname, "src/utils"),
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@styles": path.resolve(__dirname, "src/styles"),
-      "@hooks": path.resolve(__dirname, "src/hooks"),
-    },
-  },
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
+			'@assets': path.resolve(__dirname, 'src/assets'),
+			'@sass': path.resolve(__dirname, 'src/sass'),
+			'@styles': path.resolve(__dirname, 'src/styles'),
+			'@hooks': path.resolve(__dirname, 'src/hooks'),
+			'@models': path.resolve(__dirname, 'src/models'),
+			'@data': path.resolve(__dirname, 'src/data')
+		}
+	}
 })
