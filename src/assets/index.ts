87dbf2d278907f6526fe73776de5414f6f3d4cf7
Santiago Hernandez <santiagooheernandez@gmail.com>
2022-12-21 10:33:53 -0400

    Chore: add project's assets

---
+++ src/assets/index.ts
@@ -0,0 +1,47 @@
+import BarGroup from './bar-group.svg'
+import Beemo3d from './beemo-3d.svg'
+import BeemoIcon from './beemo-icon.svg'
+import BG from './bg.svg'
+import blob from './blob.svg'
+import ColumnVertical from './column-vertical.svg'
+import Component from './component.svg'
+import Facebook from './facebook.png'
+import FlexAlignSpaced from './flex-align-spaced.svg'
+import Google from './google.png'
+import Instagram from './instagram.png'
+import IsoLogo from './isologo.svg'
+import Layers from './layers.png'
+import Layout from './layout.svg'
+import Lightning from './lightning.svg'
+import LogoBg from './logo-bg.svg'
+import Microsoft from './microsoft.png'
+import Paypal from './paypal.png'
+import Shopify from './shopify.png'
+import Stars from './stars.svg'
+import Twitter from './twitter.png'
+import UserProfileCirle from './user-profile-circle.svg'
+
+export {
+	BarGroup,
+	Beemo3d,
+	blob,
+	ColumnVertical,
+	Component,
+	Facebook,
+	Instagram,
+	Microsoft,
+	Twitter,
+	Paypal,
+	Shopify,
+	Google,
+	FlexAlignSpaced,
+	IsoLogo,
+	Layers,
+	Layout,
+	Lightning,
+	LogoBg,
+	Stars,
+	UserProfileCirle,
+	BeemoIcon,
+	BG
+}
